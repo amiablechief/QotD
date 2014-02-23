dd4a8104148f033887ff87e0406ce5a0876b644b
Kulin <kulinp@hotmail.com>
2014-02-23 13:31:13 -0500

    Fixed app crash. Published version 1.02 on the play store corresponding to the fix.

--- src/com/seriousplay/qotd/MainActivity.java
+++ src/com/seriousplay/qotd/MainActivity.java
@@ -40,13 +40,13 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onStart() {
 		super.onStart();
-		EasyTracker.getInstance().activityStart(this);
+		EasyTracker.getInstance(this).activityStart(this);
 	}
 	
 	@Override
 	protected void onStop() {
 		super.onStop();
-		EasyTracker.getInstance().activityStop(this);
+		EasyTracker.getInstance(this).activityStop(this);
 	}
 	
 	//Code to save state on orientation change
